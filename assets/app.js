5e541c31f0cd8e211076fc7bcf580d3d841de810
NoaGhillain <noa.ghillain@student.heaj.be>
2026-06-01 16:57:40 +0200

    new version
    nouvelle version avec une page html en +

---
+++ assets/app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.querySelector('.accueilTitle').classList.add('visible');
+    }, 500);
+});
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.querySelector('.v1').classList.add('visible');
+    }, 800);
+});
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.querySelector('.v2').classList.add('visible');
+    }, 1100);
+});
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.querySelector('.v3').classList.add('visible');
+    }, 1400);
+});
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.querySelector('.v4').classList.add('visible');
+    }, 1700);
+});
+
