2a7734068d5ec1a1a295c13635ddd661c03c2d65
NoaGhillain <noa.ghillain@student.heaj.be>
2026-06-03 13:51:59 +0200

    transition fait
    transition fait

--- assets/app.js
+++ assets/app.js
@@ -30,3 +30,122 @@ window.addEventListener('load', () => {
     }, 1700);
 });
 
+
+
+
+const visu = document.querySelector(".visuel__link");
+const overlay = document.querySelector(".transition");
+
+visu.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    const url = this.href;
+
+    overlay.classList.add("active");
+
+    overlay.addEventListener("transitionend", function(){
+
+        // attendre 2 secondes
+        setTimeout(() => {
+            sessionStorage.setItem("transition", "true");
+            window.location.href = url;
+        }, 2000);
+
+    }, { once:true });
+
+});
+
+
+const visu2 = document.querySelector(".visu2");
+const overlay2 = document.querySelector(".transition2");
+
+visu2.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    const url = this.href;
+
+    overlay2.classList.add("active");
+
+    overlay2.addEventListener("transitionend", function(){
+
+        // attendre 2 secondes
+        setTimeout(() => {
+            sessionStorage.setItem("transition", "true");
+            window.location.href = url;
+        }, 2000);
+
+    }, { once:true });
+
+});
+
+const visu3 = document.querySelector(".visu3");
+const overlay3 = document.querySelector(".transition3");
+
+visu3.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    const url = this.href;
+
+    overlay3.classList.add("active");
+
+    overlay3.addEventListener("transitionend", function(){
+
+        // attendre 2 secondes
+        setTimeout(() => {
+            sessionStorage.setItem("transition", "true");
+            window.location.href = url;
+        }, 2000);
+
+    }, { once:true });
+
+});
+
+const visu4 = document.querySelector(".visu4");
+const overlay4 = document.querySelector(".transition4");
+
+visu4.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    const url = this.href;
+
+    overlay4.classList.add("active");
+
+    overlay4.addEventListener("transitionend", function(){
+
+        // attendre 2 secondes
+        setTimeout(() => {
+            sessionStorage.setItem("transition", "true");
+            window.location.href = url;
+        }, 2000);
+
+    }, { once:true });
+
+});
+
+const visu5 = document.querySelector(".visu5");
+const overlay5 = document.querySelector(".transition5");
+
+visu5.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    const url = this.href;
+
+    overlay5.classList.add("active");
+
+    overlay5.addEventListener("transitionend", function(){
+
+        // attendre 2 secondes
+        setTimeout(() => {
+            sessionStorage.setItem("transition", "true");
+            window.location.href = url;
+        }, 2000);
+
+    }, { once:true });
+
+});
+
